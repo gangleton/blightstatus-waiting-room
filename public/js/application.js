71ef68aa83bf5a8e4ede10ca4942f5a81b5fc644
Eddie A Tejeda <eddie@codeforamerica.org>
2012-09-19 21:39:18 -0700

    add easter egg! header is moveable. like a... map

--- public/js/application.js
+++ public/js/application.js
@@ -16,18 +16,71 @@ $(document).ready(function(){
      }else{
        $('.error').text('Please enter a valid email address');
      };
-  });
+  }) ;
 
 
- $('.toggle-btn').toggle(function(e){
-    e.preventDefault();
-    $(this).siblings('.dropdown').show();
- }, function(e){
-    e.preventDefault();
-    $(this).siblings('.dropdown').hide();
- });
 
-  $('.carousel').carousel({
-    interval: false
-  });
+  var $bg = $('.header-logo'),
+      elbounds = {
+          w: parseInt($bg.width()),
+          h: parseInt($bg.height())
+      },
+      bounds = {w: 1003 - elbounds.w, h: 593 - elbounds.h},
+      origin = {x: 0, y: 0},
+      start = {x: 0, y: 0},
+      movecontinue = false;
+  
+  var move = function(e){
+      var inbounds = {x: false, y: false},
+          offset = {
+              x: start.x - (origin.x - e.clientX),
+              y: start.y - (origin.y - e.clientY)
+          };
+      
+      // data.value = 'X: ' + offset.x + ', Y: ' + offset.y;
+      
+      inbounds.x = offset.x < 0 && (offset.x * -1) < bounds.w;
+      inbounds.y = offset.y < 0 && (offset.y * -1) < bounds.h;
+      
+      if (movecontinue && inbounds.x && inbounds.y) {
+          start.x = offset.x;
+          start.y = offset.y;
+          
+          $(this).css('background-position', start.x + 'px ' + start.y + 'px');
+      }
+      
+      origin.x = e.clientX;
+      origin.y = e.clientY;
+      
+      e.stopPropagation();
+      return false;
+  }
+  
+  var handle = function(e){
+      movecontinue = false;
+      $bg.unbind('mousemove', move);
+      
+      if (e.type == 'mousedown') {
+          origin.x = e.clientX;
+          origin.y = e.clientY;
+          movecontinue = true;
+          $bg.bind('mousemove', move);
+      } else {
+          $(document.body).focus();
+      }
+      
+      e.stopPropagation();
+      return false;
+  }
+  
+  var reset = function(){
+      start = {x: 0, y: 0};
+      $(this).css('backgroundPosition', '0 0');
+  }
+  
+  $bg.bind('mousedown mouseup mouseleave', handle);
+  $bg.bind('dblclick', reset);
+
+
+
 });
